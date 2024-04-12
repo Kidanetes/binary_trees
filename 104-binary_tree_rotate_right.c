b13ec8c0d59466d3ea1843b66af027adc699b296
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2024-04-12 09:48:11 +0300

    Rotated node has right child

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -16,6 +16,7 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 	tmp1 = tree->left;
 	tree->parent = tmp1;
 	tree->left = tmp1->right;
+	tree->right->parent = tree;
 	tmp1->right = tree;
 	tmp1->parent = NULL;
 	return (tmp1);
